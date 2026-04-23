a8a1ebcbca7d232bfeadcec7560dd700f4197be3
Kruthika1112 <kruthikabowjenku@gmail.com>
2026-04-23 12:42:17 +0530

    new

--- ExpTrack/src/main/java/com/expense/ExpTrack/TransactionController.java
+++ ExpTrack/src/main/java/com/expense/ExpTrack/TransactionController.java
@@ -2,6 +2,7 @@ package com.expense.ExpTrack;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -24,44 +25,5 @@ public class TransactionController {
     @DeleteMapping("/deleteTransaction/{id}")
     public void deleteTransaction(@PathVariable Long id) {
         repo.deleteById(id);
-    }
-
-    // ✅ TOTAL
-    @GetMapping("/total")
-    public double total() {
-        return repo.findAll()
-                .stream()
-                .mapToDouble(Transaction::getAmount)
-                .sum();
-    }
-
-    // ✅ MONTHLY
-    @GetMapping("/monthly")
-    public double monthly() {
-        return repo.findAll()
-                .stream()
-                .filter(t -> t.getDate().getMonthValue() == java.time.LocalDate.now().getMonthValue())
-                .mapToDouble(Transaction::getAmount)
-                .sum();
-    }
-
-    // ✅ PREDICTION
-    @GetMapping("/predict")
-    public double predict() {
-        List<Transaction> list = repo.findAll();
-        if (list.isEmpty()) return 0;
-
-        double total = list.stream()
-                .mapToDouble(Transaction::getAmount)
-                .sum();
-
-        return total / list.size();
-    }
-
-    // ✅ ALERT
-    @GetMapping("/budget-alert")
-    public String alert() {
-        double total = monthly();
-        return total > 5000 ? "⚠️ Budget exceeded!" : "✅ Within budget";
     }
 }
